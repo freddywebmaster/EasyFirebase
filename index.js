6e899aa759033584b7912a5da43f3355a5b60eea
freddy <freddy12gutierrez@gmail.com>
2022-02-06 20:19:43 -0600

    init app change

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import { initializeApp } from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 import 'firebase/storage';
 
 const EaseFirebase = ( appCredentials ) => {
     //initialize firebase app
-    const app = (!firebase.apps.length) ? 
-    firebase.initializeApp(appCredentials) : 
-    firebase.app();
+    const app = initializeApp(appCredentials);
 
     // return instace whit app configurated
     return {
