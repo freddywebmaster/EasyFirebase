95ccf3462e776195ca7abdaa4e9131f005a9f63f
freddy <freddy12gutierrez@gmail.com>
2022-02-07 04:13:11 -0600

    configs fixed

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const firebase = require('firebase/compat/app');
-require('firebase/compat/firestore');
-require('firebase/compat/auth');
-require('firebase/storage');
+const { initializeApp } = require("firebase/app");
+require("firebase/compat/firestore");
+require("firebase/compat/auth");
+require("firebase/storage");
 
-const EaseFirebase = ( appCredentials ) => {
-    //initialize firebase app
-    const app = (!firebase.apps.length) ? 
-    firebase.initializeApp(appCredentials) : 
-    firebase.app();
+const EaseFirebase = (appCredentials) => {
+  //initialize firebase app
+  const app = initializeApp(appCredentials);
 
-    // return instace whit app configurated
-    return {
-        app
-    }
-}
+  // return instace whit app configurated
+  return {
+    app
+  };
+};
 module.exports = EaseFirebase;
