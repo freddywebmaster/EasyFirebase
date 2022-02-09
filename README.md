bb04121ed48f43179b12a7ce51ca2aa43b9dc84b
freddy <freddy12gutierrez@gmail.com>
2022-02-09 12:38:30 -0600

    docs updated and added fn getMultipleCollection in firestore

--- README.md
+++ README.md
@@ -1,68 +1,30 @@
 # EasyFirebaseReact
-This library is being created in order to facilitate repetitive functions in firebase projects, **easy-firebase-react** makes it easier for us to manage users, manage firestore, and upload files.
+
+This library is being created in order to facilitate repetitive functions in firebase projects, **easy-firebase-react** makes it easier for us to manage users, firestore database, and upload files to storage.
 **This library is not finished but you can test it for now :)**
 
 ### Content list
+
 - Installation
 - Project Settings
+- Usage
+- Manage Errros
 - Firebase Authentication
-- Docs not ready
+- Firebase Firestore
+- Firebase Storage
 
 ## Installation
+
 `npm install easy-firebase-react firebase`
 only install firebase if you dont have.
 
 ## Configurations
 
-1. Import EasyFirebase from root dir of the lib
-
-`import EasyFirebase from "easy-firebase-react";`
-
-2. import the classes from functions dir of the lib, this classes contain the functions
-
-`import { Auth, Firestore, Storage } from "easy-firebase-react/functions";`
-
-3. inizialize the app with your firebase credentials
-
-```javascript
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
-
-//pass your credentials to EasyFirebase and this return your app instance
-const { app } = EasyFirebase(firebaseConfig);
-```
-
-4. instace the classes with your app inizialize in the constructor
-
-```javascript
-const auth = new Auth(app);
-const firestore = new Firestore(app);
-const storage = new Storage(app);
-```
-
-5. Export your instances to start use the functions
-
-```javascript
-export {
-    auth, firestore, storage
-}
-```
-
-6. The final configuration would look like this ...
+create a config file like this.. and only need set your credentials.
 
 ```javascript
 import EasyFirebase from "easy-firebase-react"; //to initialize your app firebase
-import {
-    Auth,
-    Firestore,
-    Storage
-} from "easy-firebase-react/functions"; //classes with functions
+import { Auth, Firestore, Storage } from "easy-firebase-react/functions"; //classes with functions
 
 //your credentials app
 const firebaseConfig = {
@@ -71,7 +33,7 @@ const firebaseConfig = {
   projectId: "",
   storageBucket: "",
   messagingSenderId: "",
-  appId: ""
+  appId: "",
 };
 
 //pass your credentials to EasyFirebase and this return your app instance
@@ -83,9 +45,7 @@ const firestore = new Firestore(app);
 const storage = new Storage(app);
 
 //export your instances
-export {
-    auth, firestore, storage
-}
+export { auth, firestore, storage };
 ```
 
 ## Usage
@@ -94,96 +54,117 @@ export {
 
 `import { auth, firestore, storage } from 'YOUR_CONFIG_FILE'`
 
-2. use the functions avalibles...
+2. use the functions avalibles with async/await...
 
 ```javascript
-const res = firestore.addDoc('products', {
-    name: 'tv', price: 500
+const newDoc = await firestore.addDoc("products", {
+  name: "tv smart 3",
+  price: 500,
 });
+//Manage Errors
+if (newDoc.error) {
+  console.log(`error adding doc: ${newDoc.message}`);
+} else {
+  console.log("doc added: ", newDoc);
+}
+```
 
-//the functions returns errors or another responses
+# Manage Erros
+
+- all functions of this library return firebase errors or custom errors
+- for better information you can log the response to the console
+
+```javascript
+const res = await auth.loginGithub();
+
+//manage errors
+if (res.error) return alert(res.message);
+if (!res.error) return alert(res.message);
 console.log(res);
 ```
 
 # Firebase Authentication
 
 ## Create Account With Email
+
 1. Remenber activate the email auth options in your firebase console
 
 2. make async function like this..
 
 ```javascript
-import { auth } from 'YOUR_CONFIG_FILE';
+import { auth } from "YOUR_CONFIG_FILE";
 
 const onSubmit = async () => {
   const res = await auth.createAccount(
-    'email@email.com',
-    'pass1234',
-    'NameUser' //name user is optional
+    "email@email.com",
+    "pass1234",
+    "NameUser" //name user is optional
   );
   //manage errors
-  if(res.error) return alert(res.message);
-  if(!res.error) return alert(res.message);
-}
+  if (res.error) return alert(res.message);
+  if (!res.error) return alert(res.message);
+};
 ```
 
 ## Login With Email Account
+
 1. Remenber activate the email auth options in your firebase console
 
 2. make async function like this..
 
 ```javascript
-import { auth } from 'YOUR_CONFIG_FILE';
+import { auth } from "YOUR_CONFIG_FILE";
 
 const onSubmit = async () => {
-  const res = await auth.loginAccount(
-    'email@email.com',
-    'pass1234'
-  );
+  const res = await auth.loginAccount("email@email.com", "pass1234");
   //manage errors
-  if(res.error) return alert(res.message);
-  if(!res.error) return alert(res.message);
-}
+  if (res.error) return alert(res.message);
+  if (!res.error) return alert(res.message);
+};
 ```
 
 ## Login With Google Provider
+
 1. Remenber activate the google auth options in your firebase console
 
 2. make async function like this..
 
 ```javascript
-import { auth } from 'YOUR_CONFIG_FILE';
+import { auth } from "YOUR_CONFIG_FILE";
 
 const onClickButton = async () => {
   const res = await auth.loginGoogle();
 
   //manage errors
-  if(res.error) return alert(res.message);
-  if(!res.error) return alert(res.message);
-}
+  if (res.error) return alert(res.message);
+  if (!res.error) return alert(res.message);
+};
 ```
 
 ## Login With Github Provider
+
 1. Remenber activate the github auth options in your firebase console and set config needs in github. Read more in [firebase docs auth with github](https://firebase.google.com/docs/auth/web/github-auth)
 
 2. make async function like this..
 
 ```javascript
-import { auth } from 'YOUR_CONFIG_FILE';
+import { auth } from "YOUR_CONFIG_FILE";
 
 const onClickButton = async () => {
   const res = await auth.loginGithub();
 
   //manage errors
-  if(res.error) return alert(res.message);
-  if(!res.error) return alert(res.message);
-}
+  if (res.error) return alert(res.message);
+  if (!res.error) return alert(res.message);
+};
 ```
 
 # Manage User Authenticated
+
 you can manage the current user auth with a custom hook from EasyFirebase lib
 
 ## Example
+
 ```javascript
 import { useAuth } from 'easy-firebase-react/hooks';
 
@@ -202,10 +183,86 @@ const HomeView = () => {
 ```
 
 ## Close Session
+
 to close a session you can use the next function
 
 ```javascript
-import { auth } from 'YOUR_CONFIG_FILE';
+import { auth } from "YOUR_CONFIG_FILE";
 
 auth.closeSession();
+```
+
+# Firebase Firestore
+
+## Add new document
+
+- you can create a new document with the 'addDoc' function
+- the id is automatically generated but if you need a custom id you can pass it as the third parameter
+
+```javascript
+const newDoc = await firestore.addDoc("products", {
+  name: "tv smart 3",
+  price: 500,
+  active: true,
+});
+//Manage Errors
+if (newDoc.error) {
+  console.log(`error adding doc: ${newDoc.message}`);
+} else {
+  console.log("doc added: ", newDoc);
+}
+```
+
+## Update or Replace existend document
+
+- You can update a document with the 'updateDoc' function
+- by default it does a data merge, but you can replace the entire document by passing the value of false in the fourth parameter
+
+```javascript
+const update = await firestore.updateDoc("products", "a7BTAeEnYbbXi2vCjoql", {
+  price: 600,
+});
+console.log(update);
+```
+
+## Delete existend document
+
+- you can delete a document passing it the collection and the id of the document
+
+```javascript
+const docDelete = await firestore.deleteDoc("products", "89btftioa87SQjEIswjP");
+console.log(docDelete);
+```
+
+## Add data in array
+
+- adds data to an array, it can be anything, a string, object, array, etc.
+- if the array does not exist yet it will create it and add the data
+
+```javascript
+const addA = await firestore.addInArray(
+  "products", //collection
+  "8RvHtf437D4clakqBtNO", //idDoc
+  "categories", //array field
+  "category1" // data to add
+);
+console.log(addA);
+```
+
+## Add data in array
+
+- adds data to an array, it can be anything, a string, object, array, etc.
+- if the array does not exist yet it will create it and add the data
+
+```javascript
+const delA = await firestore.deleteInArray(
+  "products", //collection
+  "8RvHtf437D4clakqBtNO", //docId
+  "categories", //Array field
+  { //All data of document to delete
+    description: "desc of category",
+    name: "category one",
+  }
+);
+console.log(addA);
 ```
--- functions/Firestore.js
+++ functions/Firestore.js
@@ -1,207 +1,239 @@
 const {
-    collection,
-    addDoc,
-    getDocs,
-    setDoc,
-    doc,
-    arrayUnion,
-    updateDoc,
-    arrayRemove,
-    increment,
-    deleteDoc,
-    deleteField,
-    getDoc,
-    query,
-    where,
-    onSnapshot,
-    getFirestore
+  collection,
+  addDoc,
+  getDocs,
+  setDoc,
+  doc,
+  arrayUnion,
+  updateDoc,
+  arrayRemove,
+  increment,
+  deleteDoc,
+  deleteField,
+  getDoc,
+  query,
+  where,
+  onSnapshot,
+  getFirestore,
 } = require("firebase/firestore");
 
-
 class Firestore {
-    constructor(app) {
-        this.db = getFirestore(app);
+  constructor(app) {
+    this.db = getFirestore(app);
+  }
+
+  async addDoc(col, data, id = "") {
+    try {
+      if (id.trim() === "") {
+        const docRef = await addDoc(collection(this.db, col), data);
+        data.id = docRef.id;
+        return data;
+      } else {
+        await setDoc(doc(this.db, col, id), data, { merge: true });
+        data.id = id;
+        return data;
+      }
+    } catch (e) {
+      return { error: true, message: e.message };
     }
+  }
 
-    async addDoc(col, data, id = '') {
-        try {
-            if (id.trim() === '') {
-                const docRef = await addDoc(collection(this.db, col), data);
-                data.id = docRef.id;
-                return data;
-            } else {
-                await setDoc(doc(this.db, col, id), data, {merge: true});
-                data.id = id;
-                return data;
-            }
-        } catch (e) {
-            return {error: true, message: e.message};
-        }
+  async updateDoc(col, docId, newData, merge = true) {
+    try {
+      const docRef = doc(this.db, col, docId);
+      setDoc(docRef, newData, { merge: merge });
+      newData.id = docId;
+      return newData;
+    } catch (e) {
+      return { error: true, message: e.message };
     }
+  }
 
-    async updateDoc(col, docId, newData, merge = true) {
-        try {
-            const docRef = doc(this.db, col, docId);
-            setDoc(docRef, newData, { merge: merge });
-            newData.id = docId;
-            return newData;
-        } catch (e) {
-            return {error: true, message: e.message};
-        }
+  async deleteDoc(col, idDoc) {
+    const docSearch = await this.getDocById(col, idDoc);
+    if (docSearch.error) return { error: true, message: "doc not exist" };
+    try {
+      await deleteDoc(doc(this.db, col, idDoc));
+      return { error: false, message: `doc deleted: ${idDoc}` };
+    } catch (e) {
+      return { error: true, message: e.message };
     }
+  }
 
-    async deleteDoc(col, idDoc) {
-        const docSearch = await this.getDocById(col, idDoc);
-        if(docSearch.error) return {error: true, message: 'doc not exist'}
-        try {
-            await deleteDoc(doc(this.db, col, idDoc));
-            return { error: false, message: 'not errors' };
-        } catch (e) {
-            return {error: true, message: e.message};
-        }
+  async addInArray(col, id, field, data) {
+    try {
+      const docRef = doc(this.db, col, id);
+      await updateDoc(docRef, {
+        [field]: arrayUnion(data),
+      });
+
+      return { error: false };
+    } catch (e) {
+      return { error: true, message: e.message };
     }
+  }
 
-    async addInArray(col, id, field, data) {
-        try {
-            const docRef = doc(this.db, col, id);
-            await updateDoc(docRef, {
-                [field]: arrayUnion(data)
-            });
+  async deleteInArray(col, id, field, data) {
+    try {
+      const docRef = doc(this.db, col, id);
+      await updateDoc(docRef, {
+        [field]: arrayRemove(data),
+      });
 
-            return { error: false };
-        } catch (e) {
-            return {error: true, message: e.message};
-        }
+      return { error: false };
+    } catch (e) {
+      return { error: true, message: e.message };
     }
+  }
 
-    async deleteInArray(col, id, field, data) {
-        try {
-            const docRef = doc(this.db, col, id);
-            await updateDoc(docRef, {
-                [field]: arrayRemove(data)
-            });
-
-            return { error: false };
-        } catch (e) {
-            return {error: true, message: e.message};
-        }
+  async incrementOrDecrementNumber(col, docId, field, number) {
+    try {
+      const docRef = doc(this.db, col, docId);
+      await updateDoc(docRef, {
+        [field]: increment(number),
+      });
+      return { error: false };
+    } catch (e) {
+      return { error: true, message: e.message };
     }
+  }
 
-    async incrementOrDecrementNumber(col, docId, field, number) {
-        try {
-            const docRef = doc(this.db, col, docId);
-            await updateDoc(docRef, {
-                [field]: increment(number),
-            });
-            return { error: false };
-        } catch (e) {
-            return {error: true, message: e.message};
-        }
+  async deleteFieldInDoc(col, docId, field) {
+    try {
+      const docRef = doc(this.db, col, docId);
+      await updateDoc(docRef, {
+        [field]: deleteField(),
+      });
+      return { error: false };
+    } catch (e) {
+      return { error: true, message: e.message };
     }
+  }
 
-    async deleteFieldInDoc(col, docId, field) {
-        try {
-            const docRef = doc(this.db, col, docId);
-            await updateDoc(docRef, {
-                [field]: deleteField(),
-            });
-            return { error: false };
-        } catch (e) {
-            return {error: true, message: e.message};
-        }
+  async getDocById(col, id) {
+    const docRef = doc(this.db, col, id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      let result = docSnap.data();
+      result.id = docSnap.id;
+      return result;
+    } else {
+      return { error: true, message: "doc not exist" };
     }
+  }
 
-    async getDocById(col, id) {
-        const docRef = doc(this.db, col, id);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-            let result = docSnap.data();
-            result.id = docSnap.id;
-            return result;
-        } else {
-            return {error: true, message: 'doc not exist'};
-        }
+  async getDocsByParam(col, field, condition, param) {
+    try {
+      const q = query(collection(this.db, col), where(field, condition, param));
+      const querySnapshot = await getDocs(q);
+      const result = [];
+      await querySnapshot.forEach((doc) => {
+        let item = doc.data();
+        item.id = doc.id;
+        result.push(item);
+      });
+
+      return result;
+    } catch (error) {
+      return { error: true, message: error };
     }
+  }
 
-    async getDocsByParam(col, field, condition, param) {
-        const q = query(collection(this.db, col), where(field, condition, param));
-        const querySnapshot = await getDocs(q);
+  async getAllInCollection(col) {
+    try {
+      const querySnapshot = await getDocs(collection(this.db, col));
+      const result = [];
+      await querySnapshot.forEach((doc) => {
+        let item = doc.data();
+        item.id = doc.id;
+        result.push(item);
+      });
+      return result;
+    } catch (error) {
+      return { error: true, message: error };
+    }
+  }
+
+  async getDocByIdInRealTime(col, id, callBack) {
+    try {
+      return onSnapshot(doc(this.db, col, id), function (doc) {
+        callBack(doc.data());
+      });
+    } catch (error) {
+      return { error: true, message: error };
+    }
+  }
+
+  async getDocsByParamInRealTime(col, field, condition, param, callBack) {
+    try {
+      const q = query(collection(this.db, col), where(field, condition, param));
+      return onSnapshot(q, (querySnapshot) => {
         const result = [];
-        await querySnapshot.forEach((doc) => {
-            let item = doc.data();
-            item.id = doc.id;
-            result.push(item);
+        querySnapshot.forEach((doc) => {
+          let item = doc.data();
+          item.id = doc.id;
+          result.push(item);
         });
-
-        return result;
+        callBack(result);
+      });
+    } catch (error) {
+      return { error: true, message: error };
     }
+  }
 
-    async getAllInCollection(col) {
-        const querySnapshot = await getDocs(collection(this.db, col));
-        const result = [];
-        await querySnapshot.forEach((doc) => {
-            let item = doc.data();
-            item.id = doc.id;
-            result.push(item);
-        });
-        return result;
+  async arrayContainsAny(col, field, array) {
+    try {
+      const q = query(
+        collection(this.db, col),
+        where(field, "array-contains-any", array)
+      );
+      const querySnapshot = await getDocs(q);
+      const result = [];
+      await querySnapshot.forEach((doc) => {
+        let item = doc.data();
+        item.id = doc.id;
+        result.push(item);
+      });
+
+      return result;
+    } catch (error) {
+      return { error: true, message: error };
     }
+  }
 
-    async getDocByIdInRealTime(col, id, callBack) {
-        try {
-            return onSnapshot(doc(this.db, col, id), function (doc) {
-                callBack(doc.data());
-            });
-        } catch (error) {
-            return error;
-        }
+  async arrayContains(col, field, value) {
+    try {
+      const q = query(
+        collection(this.db, col),
+        where(field, "array-contains", value)
+      );
+      const querySnapshot = await getDocs(q);
+      const result = [];
+      await querySnapshot.forEach((doc) => {
+        let item = doc.data();
+        item.id = doc.id;
+        result.push(item);
+      });
+
+      return result;
+    } catch (error) {
+      return { error: true, message: error };
     }
+  }
 
-    async getDocsByParamInRealTime(
-        col,
-        field,
-        condition,
-        param,
-        callBack
-    ) {
-        const q = query(collection(this.db, col), where(field, condition, param));
-        return onSnapshot(q, (querySnapshot) => {
-            const result = [];
-            querySnapshot.forEach((doc) => {
-                let item = doc.data();
-                item.id = doc.id;
-                result.push(item);
-            });
-            callBack(result);
-        });
+  async getMultipleCollections(arrayCollections) {
+    try {
+      let results = {};
+
+      await arrayCollections.map(async (col) => {
+        const res = await this.getAllInCollection(col);
+        if (!res.error) results[col] = res;
+      });
+      return results;
+    } catch (error) {
+      return { error: true, message: error };
     }
-
-    async arrayContainsAny(col, field, array){
-        const q = query(collection(this.db, col), where(field , 'array-contains-any', array));
-        const querySnapshot = await getDocs(q);
-        const result = [];
-        await querySnapshot.forEach((doc) => {
-            let item = doc.data();
-            item.id = doc.id;
-            result.push(item);
-        });
-
-        return result;
-    }
-
-    async arrayContains(col, field, value){
-        const q = query(collection(this.db, col), where(field , 'array-contains', value));
-        const querySnapshot = await getDocs(q);
-        const result = [];
-        await querySnapshot.forEach((doc) => {
-            let item = doc.data();
-            item.id = doc.id;
-            result.push(item);
-        });
-
-        return result;
-    }
-
+  }
 }
 
 module.exports = Firestore;
